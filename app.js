ef63492004db7d08a26bf27916d473f378b847f9
Ragz-no <70691805+Ragz-no@users.noreply.github.com>
2022-04-25 10:55:22 +0200

    modified .gitingore app.js style.css header.ejs

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+
+app.use(express.static("public"));
+
 // app.get("/", function(req, res){
 //     res.sendFile(__dirname + "/index.html");
 // });
