08f17da965e1f7946099fda987bf1d025a906b3f
Ragz-no <70691805+Ragz-no@users.noreply.github.com>
2022-04-25 10:39:17 +0200

    new changes

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+// app.get("/", function(req, res){
+//     res.sendFile(__dirname + "/index.html");
+// });
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-});
+    res.render("home");
+  });
+  
 
 app.listen(6969, function(){
     console.log("Server started on port http://localhost:6969/");
