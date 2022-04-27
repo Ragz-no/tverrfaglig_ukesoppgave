d5cebdf7fde4a10526df141cabee8bb35cc462a5
Ragz-no <70691805+Ragz-no@users.noreply.github.com>
2022-04-27 10:48:32 +0200

    modified several files for google auth to work

--- app.js
+++ app.js
@@ -6,21 +6,23 @@ const ejs = require("ejs");
 
 
 const app = express();
+const PORT = process.env.PORT || 6969;
 
 app.set('view engine', 'ejs');
 
 
 app.use(express.static("public"));
 
-// app.get("/", function(req, res){
-//     res.sendFile(__dirname + "/index.html");
-// });
-
-app.get("/", function(req, res){
+app.get("/", (req, res) =>{
     res.render("home");
   });
   
+app.get("/login", function(req, res){   
+    res.render("login");
+});
 
-app.listen(6969, function(){
+
+
+app.listen(PORT, () =>{
     console.log("Server started on port http://localhost:6969/");
 });
